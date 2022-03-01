43580bcee9c66e2abcee39078e5bab25b57786ca
ChristineJackson464 <mom2mb464@gmail.com>
2022-03-01 09:28:27 -0500

    submitting JSBasicsExercise w/ Bonus

---
+++ script.js
@@ -0,0 +1,66 @@
+// 1. Use a console log to have your favorite number display in the console/terminal
+console.log("1"); // 1
+
+// 2. Use a comment to type out the 5 main data types (AKA Primitives)
+/* The 5 main data types:
+        1. Primitives
+        2. Strings
+        3. Booleans
+        4. Null
+        5. Undefined
+*/
+
+// 3. Use a comment to type out the 6 operator symbols.
+/* The 6 operator symbols:
+        1. + (ADDITION)
+        2. - (SUBTRACTION)
+        3. * (MULTIPLICATION)
+        4. / (DIVISION)
+        5. ** (EXPONENTIATION)
+        6. % (MODULUS)
+*/
+
+// 4. Use modulus to get a remainder of 1 from the number 
+console.log(10%3); //1
+
+// 5. (There are two answers for this one. Please provide both. Also, please type them both out. Do not just type out the two numbers)
+//  Using single quotes at the start and end of the string have the words I'M MARY POPPINS, Y'ALL! display in the terminal/console (Must use a console log for this one.)
+console.log('I\'M MARY POPPINS, Y\'ALL!'); //I'M MARY POPPINS, Y'ALL!
+
+// 6. Using Concatenation combine the following strings into one string. First string: "Hawk". Second string: "eye".
+console.log("Hawk" + "eye");
+
+// 7. Create a string for your first name and also your last name. Use the length property to add them together. 
+console.log('Christine'.length + 'Jackson'.length); //16
+
+// 8. Use index on the following string to select the lowercase "e" at the end of the string. "Avengers: Endgame". (Use a console log to display it in the console/terminal)
+console.log("Avengers: Endgame".length); //17
+console.log("Avengers: Endgame" [16]); //e
+
+// BONUS
+
+// B-1. Create a string that displays only a backslash when it is displayed in the console/terminal
+console.log('I\\\'M MARY POPPINS, Y\'ALL!'); //I\'M MARY POPPINS, Y'ALL!
+
+// B-2. Use the following numbers in the order shown to get a result of the 616. Numbers are 5, 4, and 9 
+console.log((5**4)-9);
+
+//B-3. Use the following strings in order to display a result of Zero in the console/terminal using the length property. (NOTE: There are multiple answers. For an extra bonus try to use modulo
+console.log(("I".length+"Am".length+"Iron".length+"Man".length)%10); //0
+
+//B-4. Using Index on each of the following strings, have AVENGERS display vertically in the console/terminal (Strings: "CAP" "VISION" "OKOYE" "NICK FURY" "GROOT" "DOCTOR STRANGE" "THOR" "STAR-LORD"). (NOTE: Need to create a separate console log for each string).
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[12]);
+console.log("THOR"[3]);
+console.log("STAR-LORD"[0]);
+
+// B-5. Create the following variables with the following values m=25 and c=4. Create a variable with the name of u and set the value for it by adding m and c together (NOTE: Must add actual variables NOT just 25 + 4 when setting value for the u variable).
+let m=25;
+let c=4;
+let u=c+m;
+
+
